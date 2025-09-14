c61171c540f35a7c2381b526b34433bc4e267b72
コアラ <189215038+coara-chocomaru@users.noreply.github.com>
2025-09-14 23:23:46 +0900

    Update BoardConfig.mk

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -117,8 +117,8 @@ TW_CRYPTO_FS_TYPE := "ext4"
 TW_CRYPTO_REAL_BLKDEV := "/dev/block/platform/mtk-msdc.0/11120000.msdc0/by-name/userdata"
 TW_CRYPTO_MNT_POINT := "/data"
 TW_CRYPTO_FS_OPTIONS := "nosuid,nodev,noatime,discard,noauto_da_alloc,data=ordered"
-TW_EXTRA_LANGUAGES := false
-TW_DEFAULT_LANGUAGE := en
+TW_EXTRA_LANGUAGES := true
+TW_DEFAULT_LANGUAGE := ja
 TW_USE_INPUT_EVENTS := true
 TW_INPUT_BLACKLIST := "hbtp_vm"
 TW_SCREEN_BLANK_ON_BOOT := true
